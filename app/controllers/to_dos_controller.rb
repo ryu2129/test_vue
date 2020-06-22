9bb1e0180e4616fa1d53c72e813d1aba797050cc
ryu2129 <ryu212920@gmail.com>
2020-06-22 09:59:11 +0000

    close #4

--- app/controllers/to_dos_controller.rb
+++ app/controllers/to_dos_controller.rb
@@ -1,4 +1,6 @@
 class ToDosController < ApplicationController
+  before_action :set_to_do, only: [:edit, :update]
+
   def index
     @to_dos = ToDo.all
   end
@@ -16,9 +18,25 @@ class ToDosController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @to_do.update(to_do_params)
+      redirect_to to_dos_url
+    else
+      render :edit
+    end
+  end
+
+
 private
   def to_do_params
     params.require(:to_do).permit(:title)
+  end
+
+  def set_to_do
+    @to_do = ToDo.find_by(id: params[:id])
   end
 
 end
